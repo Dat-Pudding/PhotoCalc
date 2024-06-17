7ad3d7a4a31915d7db5cfc233cd9ee9d9005912a
Josh <110232756+Dat-Pudding@users.noreply.github.com>
2024-06-17 21:32:42 +0200

    edited repository files
    - added full unity project (except 4096x-icon-png
    - renamed directories to be more precise

--- Unity_source/Assets/ScriptableObjectsOld/PatchNotes.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PatchNotes", menuName = "ScriptableObjects/Libs/Patch Notes", order = 2)]
-public class PatchNotes : ScriptableObject
-{
-    public string[] patchNotes;
-}
